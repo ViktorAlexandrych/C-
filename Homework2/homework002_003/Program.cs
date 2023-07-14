bfe958aeff742efcfa033ce0bb4d20b9d29a49c1
Alexandrych <vyrupaev.00@gmail.com>
2023-07-14 19:45:14 +1000

    create all homework

---
+++ Homework2/homework002_003/Program.cs
@@ -0,0 +1,37 @@
+﻿int Prompt(string msg)
+{
+	Console.WriteLine($"{msg}");
+	return Convert.ToInt32(Console.ReadLine());
+}
+bool weekend(int weekday)
+{
+	if (weekday > 5)
+	{
+		return true;
+	}
+	return false;
+}
+bool noweekday(int number)
+{
+	if (number > 0 && number <= 7)
+	{
+		return true;
+	}
+	else
+	{
+		Console.WriteLine("Это не день недели");
+		return false;
+	}
+}
+int weekday = Prompt("Введи день недели");
+if (noweekday(weekday))
+{
+	if (weekend(weekday))
+	{
+		Console.WriteLine("Это выходной день недели");
+	}
+	else
+	{
+		Console.WriteLine("Это будний день недели");
+	}
+}
