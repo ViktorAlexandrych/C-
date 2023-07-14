ccc1ead6afe6dc3822f1690256b4ba9c006e7ce4
Alexandrych <vyrupaev.00@gmail.com>
2023-07-14 18:20:33 +1000

    Initial

---
+++ Homework2/homework002_001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+while (true)
+{
+	string input = Console.ReadLine();
+	if (!input.Equals("Back"))
+		Console.WriteLine("{0}->{1}", input, input[1]);
+	else
+		break;
+}
