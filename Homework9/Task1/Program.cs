fb161c30c7388d72306e86ba8e16cf39dce48bbe
Alexandrych <vyrupaev.00@gmail.com>
2023-08-12 19:43:06 +1000

    This commit save Homework 9

--- Homework9/Task1/Program.cs
+++ Homework9/Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Rec(int n)
+{
+	if (n == 0)
+		return;
+	if (n > 0)
+	{
+		Console.Write($"{n} ");
+	}
+	Rec(n - 1);
+
+}
+Rec(n);
